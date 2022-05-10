c62826c4c1e66ec8bb2f4bd4d99dfac7c53fe563
FtobarV64 <ftobarv@gmail.com>
2022-05-09 21:33:09 -0400

    aditional fields commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def index
   end
   def create
-    User.create(email: params[:email])
+    User.create(email: params[:email], age: params[:age], comment: params[:comment])
   end
 end
