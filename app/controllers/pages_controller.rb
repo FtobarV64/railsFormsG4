7d0177e172fa914bf40d0d63828ba9b6e0120d0f
FtobarV64 <ftobarv@gmail.com>
2022-05-10 19:41:02 -0400

    validation commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def dashboard
+    @users = User.find_valid
+  end
+  
   def create
-    User.create(email: params[:email], age: params[:age], comment: params[:comment])
+    @user = User.create(email: params[:email], age: params[:age], comment: params[:comment])
   end
 end
